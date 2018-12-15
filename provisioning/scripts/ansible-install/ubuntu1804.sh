a847ef7bc48c38f12bf416b403407fd6f5a666bc
Varlog <varlog.err@gmail.com>
2018-12-15 12:28:01 +0200

    add ansible install provisioner

---
+++ provisioning/scripts/ansible-install/ubuntu1804.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# can be used with:
+# * ubuntu bionic
+# * linux mint tara
+
+apt-get update
+
+if which ansible >/dev/null; then
+    echo "Upgrading ansible"
+    apt-get --only-upgrade install -y ansible
+    exit 0
+fi
+
+echo "Installing software-properties-common and dirmngr"
+apt-get install -y software-properties-common dirmngr
+
+echo "Installing ansible"
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 6125E2A8C77F2818FB7BD15B93C4A3FD7BB9C367 \
+&& echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu bionic main" > /etc/apt/sources.list.d/ansible.list \
+&& echo "deb-src http://ppa.launchpad.net/ansible/ansible/ubuntu bionic main" >> /etc/apt/sources.list.d/ansible.list \
+&& apt-get update \
+&& apt-get install -y -f ansible
